a3da94ae74eec3f046ec735fb45064c5d25ce2ea
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-09-17 11:33:08 -0300

    try-catch and throw new

--- src/main/java/br/com/nicolas/financemanager/Main.java
+++ src/main/java/br/com/nicolas/financemanager/Main.java
@@ -9,11 +9,16 @@ public class Main {
         Transaction salary= new Transaction("Salario", new BigDecimal("2500.00"), TransactionType.INCOME);
         Transaction netflix = new Transaction("Netflix", new BigDecimal("50.00"), TransactionType.EXPENSE);
         Transaction spotify = new Transaction("Spotify", new BigDecimal("40.00"), TransactionType.EXPENSE);
-        Transaction teste = new Transaction("Teste", new BigDecimal("50.00"), null);
+        try {
+            Transaction google = new Transaction("Google", BigDecimal.ZERO, TransactionType.EXPENSE);
+        } catch (IllegalArgumentException Exception ) {
+           System.out.println( Exception.getMessage());
+        }
+        //Transaction teste = new Transaction("Teste", new BigDecimal("50.00"), null);
         manager.addTransaction(salary);
         manager.addTransaction(netflix);
         manager.addTransaction(spotify);
-        manager.addTransaction(teste);
+        //manager.addTransaction(teste);
         manager.listTransactions();
         System.out.println("Balance: "+manager.calculateBalance());
         System.out.println("Income: "+manager.calculateTotalByType(TransactionType.INCOME));
