1e93b70f5c185cf5f91c74bbedfdf4988641669c
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-09-17 12:17:03 -0300

    add scanner

--- src/main/java/br/com/nicolas/financemanager/Main.java
+++ src/main/java/br/com/nicolas/financemanager/Main.java
@@ -3,31 +3,28 @@ import br.com.nicolas.financemanager.model.TransactionType;
 import br.com.nicolas.financemanager.service.FinanceManager;
 import br.com.nicolas.financemanager.model.Transaction;
 import java.math.BigDecimal;
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
         FinanceManager manager=new FinanceManager();
-        Transaction salary= new Transaction("Salario", new BigDecimal("2500.00"), TransactionType.INCOME);
-        Transaction netflix = new Transaction("Netflix", new BigDecimal("50.00"), TransactionType.EXPENSE);
-        Transaction spotify = new Transaction("Spotify", new BigDecimal("40.00"), TransactionType.EXPENSE);
+        System.out.println("---- MONEY MANAGER ----");
+        
+        System.out.println("Informe a descrição: ");
+        String description=scanner.nextLine();
+        System.out.println("Informe o valor da transação: ");
+        String valueInput=scanner.nextLine();
+   
+        
         try {
-            Transaction google = new Transaction("Google", BigDecimal.ZERO, TransactionType.EXPENSE);
-        } catch (IllegalArgumentException Exception ) {
-           System.out.println( Exception.getMessage());
+            BigDecimal value = new  BigDecimal(valueInput);
+            System.out.println("Valor convertido:"+value);
+        } catch (NumberFormatException exception) {
+            System.out.println("Informe um valor de transação Válido");
         }
-        //Transaction teste = new Transaction("Teste", new BigDecimal("50.00"), null);
-        manager.addTransaction(salary);
-        manager.addTransaction(netflix);
-        manager.addTransaction(spotify);
-        //manager.addTransaction(teste);
-        manager.listTransactions();
-        System.out.println("Balance: "+manager.calculateBalance());
-        System.out.println("Income: "+manager.calculateTotalByType(TransactionType.INCOME));
-        System.out.println("Expense: "+manager.calculateTotalByType(TransactionType.EXPENSE));
 
-        Transaction foundTransaction= manager.findTransactionByDescription("Netflix");
-        if (foundTransaction!=null) {
-            System.out.println(foundTransaction);
-        }
+
+        
 
     }
 }
