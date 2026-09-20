14747bff11af29f8bbb3586006d5017c2befde20
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-09-19 23:58:26 -0300

    add while in main

--- src/main/java/br/com/nicolas/financemanager/Main.java
+++ src/main/java/br/com/nicolas/financemanager/Main.java
@@ -7,7 +7,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner (System.in);
-        FinanceManager manager=new FinanceManager();
+        boolean cadastrarOutro=true;
+        FinanceManager manager=new FinanceManager();        
+        while (cadastrarOutro) {
+
         System.out.println("---- MONEY MANAGER ----");
         
         System.out.println("Informe a descrição: ");
@@ -26,10 +29,44 @@ public class Main {
             Transaction transaction = new Transaction(description, value, type);
             manager.addTransaction(transaction);
             System.out.println("Transação cadastrada com sucesso!");
+            manager.listTransactions();
+            System.out.println("Saldo atual: "+manager.calculateBalance());
+            System.out.println("Deseja cadastrar outro outro ? (S/N)");
+            String resposta =scanner.nextLine();
+            cadastrarOutro = resposta.equalsIgnoreCase("S");
+        
 
         } catch ( IllegalArgumentException exception) {
             System.out.println("Informe valores válidos! "+exception.getMessage());
         }
+        
+    }
+        /*FinanceManager manager=new FinanceManager();
+        System.out.println("---- MONEY MANAGER ----");
+        
+        System.out.println("Informe a descrição: ");
+        String description=scanner.nextLine();
+       
+        System.out.println("Informe o valor da transação: ");
+        String valueInput=scanner.nextLine();
+        
+
+        System.out.println("Informe o tipo de transação (INCOME ou EXPENSE)");
+        String typeInput=scanner.nextLine();           
+    
+        try {
+            BigDecimal value= new BigDecimal(valueInput);
+            TransactionType type =  TransactionType.valueOf(typeInput);
+            Transaction transaction = new Transaction(description, value, type);
+            manager.addTransaction(transaction);
+            System.out.println("Transação cadastrada com sucesso!");
+            manager.listTransactions();
+            System.out.println("Saldo atual: "+manager.calculateBalance());
+
+        } catch ( IllegalArgumentException exception) {
+            System.out.println("Informe valores válidos! "+exception.getMessage());
+        }
+             */
         
         
     }
