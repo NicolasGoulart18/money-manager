59308a4ddc716f59f8118ac257693d17ad96710c
NicolasGoulart18 <nicolasgoulartt2008@gmail.com>
2026-09-20 00:02:16 -0300

    feat: support repeated transaction registration

--- src/main/java/br/com/nicolas/financemanager/Main.java
+++ src/main/java/br/com/nicolas/financemanager/Main.java
@@ -40,34 +40,7 @@ public class Main {
             System.out.println("Informe valores válidos! "+exception.getMessage());
         }
         
-    }
-        /*FinanceManager manager=new FinanceManager();
-        System.out.println("---- MONEY MANAGER ----");
-        
-        System.out.println("Informe a descrição: ");
-        String description=scanner.nextLine();
-       
-        System.out.println("Informe o valor da transação: ");
-        String valueInput=scanner.nextLine();
-        
-
-        System.out.println("Informe o tipo de transação (INCOME ou EXPENSE)");
-        String typeInput=scanner.nextLine();           
-    
-        try {
-            BigDecimal value= new BigDecimal(valueInput);
-            TransactionType type =  TransactionType.valueOf(typeInput);
-            Transaction transaction = new Transaction(description, value, type);
-            manager.addTransaction(transaction);
-            System.out.println("Transação cadastrada com sucesso!");
-            manager.listTransactions();
-            System.out.println("Saldo atual: "+manager.calculateBalance());
-
-        } catch ( IllegalArgumentException exception) {
-            System.out.println("Informe valores válidos! "+exception.getMessage());
         }
-             */
-        
-        
+       
     }
 }
